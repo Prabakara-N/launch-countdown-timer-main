54241d726841e4a1f27cc7b4c24beb95b95d2ce7
prabakaran <mspk028@gmail.com>
2022-12-21 11:43:34 +0530

    updated js

--- script/script.js
+++ script/script.js
@@ -4,13 +4,14 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
+//functions
 const countdown = () => {
-  const endDate = new Date("December 25, 2022 00:00:00").getTime();
-  const now = new Date().getTime();
+  const endDate = new Date("December 25, 2022 00:00:00").getTime(); //date future
+  const now = new Date().getTime(); // date today
 
   const difference = endDate - now;
 
-  const seconds = 1000;
+  const seconds = 1000; // 1 sec = 1000 ms
   const minutes = seconds * 60;
   const hours = minutes * 60;
   const days = hours * 24;
@@ -20,6 +21,7 @@ const countdown = () => {
   let timeMinutes = Math.floor((difference % hours) / minutes);
   let timeSeconds = Math.floor((difference % minutes) / seconds);
 
+  //condition for when timing is less than 10, the timing wil be 09...08 like that (not 9..8..7)
   timeHours = timeHours < 10 ? "0" + timeHours : timeHours;
   timeMinutes = timeMinutes < 10 ? "0" + timeMinutes : timeMinutes;
   timeSeconds = timeSeconds < 10 ? "0" + timeSeconds : timeSeconds;
